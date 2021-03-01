79fbe574df66c69214c05d0c69194761260fa55b
marmbn <marfel@mybizniche.com>
2021-03-01 13:43:53 +0800

    acf banner

--- templates/block.php
+++ templates/block.php
@@ -8,7 +8,6 @@ get_header(); ?>
     <?php
         while ( have_posts() ) : the_post();
         
-            the_title('<h1>', '</h1>');
             the_content();
 
         endwhile; // End of the loop.
