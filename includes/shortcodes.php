ca49b8f986cabef859b18fcafcf397a296ec0818
marmbn <marfel@mybizniche.com>
2021-03-03 19:45:37 +0800

    update 11

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -7,6 +7,112 @@ add_shortcode('home_url', 'mbn_shortcode_home_url');
 
 
 
+function find_us_gmap(){ 
+    wp_reset_query();
+    $office_args = array(  
+        'post_type' => 'location',
+        'posts_per_page' => -1, 
+        'post_status' => 'publish',
+        'orderby' => 'title',
+        'order' => 'ASC',
+    );
+    
+    $office_loop = new WP_Query( $office_args );
+    ?>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDac2mOtJr_IktjUhiLZYRL_xHzxRbodRE&callback=initMap&libraries=&v=weekly" defer></script>
+    <script>function initMap() {
+     
+        const myLatlng = { lat: 34.750713, lng: -111.263263 };
+    
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 8,
+            center: myLatlng,
+        });
+    
+    
+        var bounds = new google.maps.LatLngBounds();
+        var infowindow = new google.maps.InfoWindow();  
+        
+        var markers_array = new Array();
+        var locations = [];
+        var array_holder;
+        var loc_content = "";
+        var loc_ctr = 0;
+        var emp_phones = "";
+    
+        <?php while ( $office_loop->have_posts() ) : $office_loop->the_post(); ?>
+            <?php if(!empty(get_field('map_location'))): ?>
+            var loc_title = '<?php the_title(); ?>';
+            var office_lat = <?php echo get_field('map_location')['lat']; ?>; 
+            var office_lng = <?php echo get_field('map_location')['lng']; ?>; 
+
+            loc_content = '<div id="mapInfo'+loc_ctr+'" class="map_grid grid-container">';
+                
+            loc_content += '' +loc_title+ '';
+
+            loc_content += '</div>';
+
+            <?php endif; ?>
+    
+            loc_content += '</div>';
+    
+            array_holder = [loc_title, office_lat, office_lng, loc_content];
+            locations.push(array_holder);
+            
+            loc_ctr = loc_ctr+1;
+
+            <?php endif; ?>
+
+            <?php endwhile; wp_reset_postdata(); ?>
+        
+        
+        for (var i = 0; i < locations.length; i++) {
+          
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+          });
+    
+           bounds.extend(marker.position);
+    
+    
+          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+              infowindow.setContent(locations[i][3]);
+              infowindow.open(map, marker);
+              map.setCenter(this.getPosition());
+            }
+          })(marker, i));
+    
+          markers_array.push(marker);
+        }
+    
+        //now fit the map to the newly inclusive bounds
+        map.fitBounds(bounds);
+      
+    
+        //(optional) restore the zoom level after the map is done scaling
+        var listener = google.maps.event.addListener(map, "idle", function () {
+            map.setZoom(8);
+            google.maps.event.removeListener(listener);
+        });
+    
+        
+        $('a.triggerMap').each(function(i){
+            $(this).on('click', function(e){
+                //e.preventDefault();
+                google.maps.event.trigger(markers_array[i], 'click');
+            });
+        });
+    
+    
+    }</script>
+    <div id="map"></div>
+    <?php 
+    }
+    add_shortcode('display_loc_gmap', 'find_us_gmap');
+
 
 
 
--- templates/test.php
+++ templates/test.php
@@ -4,66 +4,70 @@
 
 get_header(); ?>
 
-<ul class="locations-by-tax">
+
+<div class="location_map">
+    <?= do_shortcode('[display_loc_gmap]'); ?>
+</div>
+
+<ul class="location_states">
 <?php
-$location_categories = get_terms( 'locations-cat', array(
-    'orderby'    => 'title',
-    'order'      => 'ASC',
-    'parent' => 0,
-    'hide_empty' => true
-));
+    $location_categories = get_terms( 'locations-cat', array(
+        'orderby'    => 'title',
+        'order'      => 'ASC',
+        'parent' => 0,
+        'hide_empty' => true
+    ));
 
+    foreach ( $location_categories as $location_category ) {
+        
+        $args = array(
+            'posts_per_page' => -1,
+            'tax_query' => array(
+                'relation' => 'AND',
+                array(
+                    'taxonomy' => 'locations-cat',
+                    'field' => 'slug',
+                    'terms' => $location_category->slug,
+                    'include_children' => false
+                )
+            ),
+            'post_type' => 'location',
+            'orderby' => 'title,'
+        );
+        $locations = new WP_Query( $args );
 
-foreach ( $location_categories as $location_category ) {
-    
-    $args = array(
-        'posts_per_page' => -1,
-        'tax_query' => array(
-            'relation' => 'AND',
-            array(
-                'taxonomy' => 'locations-cat',
-                'field' => 'slug',
-                'terms' => $location_category->slug,
-                'include_children' => false
-            )
-        ),
-        'post_type' => 'location',
-        'orderby' => 'title,'
-    );
-    $locations = new WP_Query( $args );
+        $getTermChildCount = count (get_term_children( $location_category->term_id, 'locations-cat' )); // Count Sub Categories
+        $postCount = $locations->found_posts; // Count Post
 
+        if($getTermChildCount < 1 and $postCount > 0) {
 
-    $getTermChildCount = count (get_term_children( $location_category->term_id, 'locations-cat' )); // Count Sub Categories
-    $postCount = $locations->found_posts; // Count Post
+        echo '<li class="state_item '. $location_category->slug .'">';
+        echo '<h6 class="state_name">'. $location_category->name .'</h6>';
+        //if($locations->found_posts > 0) {
+            echo "<ul class='location_list'>";
+            while ( $locations->have_posts() ) {
+                $locations->the_post();
+                $terms = get_the_terms( $post->ID, 'location-apps' );
 
-    if($getTermChildCount < 1 and $postCount > 0) {
+                ?>
+                <li class="city_item">
+                    <a href="#"><?php the_title(); ?></a>
+                </li>
+                <?php
+            }
+            echo "</ul>";
+        //}
+        //echo "<p class='no-results'>No location/s Found</p>";
+        echo "</li>";
 
-    echo '<li class="prodbytax-item '. $location_category->slug .'">';
-    echo '<h4 class="prodtax-head">' . $location_category->name . '</h4>';
-    //if($locations->found_posts > 0) {
-        echo "<ul class='location-list'>";
-        while ( $locations->have_posts() ) {
-            $locations->the_post();
-            $terms = get_the_terms( $post->ID, 'location-apps' );
-            
-            ?>
-            <li class="<?php if($terms) foreach( $terms as $term ) echo ' '.$term->slug; ?>">
-                <div class="location-list-item">
-                    <h6><?php the_title(); ?></h6>
-                </div>
-            </li>
-            <?php
         }
-        echo "</ul>";
-    //}
-    //echo "<p class='no-results'>No location/s Found</p>";
-    echo "</li>";
-
     }
-}
 ?>
-
 </ul>
 
+<p class="note">
+    <strong>NOTE:</strong> 
+    The map and cities below show the general geographic locations for the retail stores. Exact locations will be determined at a later date.
+</p>
 
 <?php get_footer() ?>
