2b74300962689ddf2438496d364f93a59b4c6ef5
marmbn <marfel@mybizniche.com>
2021-03-03 20:29:42 +0800

    update 12

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -6,53 +6,36 @@ function mbn_shortcode_home_url($atts = null, $content = null){
 add_shortcode('home_url', 'mbn_shortcode_home_url');
 
 
-
-function find_us_gmap(){ 
+// Find Us Location, Map
+function find_us_gmap() { 
     wp_reset_query();
-    $office_args = array(  
-        'post_type' => 'location',
-        'posts_per_page' => -1, 
-        'post_status' => 'publish',
-        'orderby' => 'title',
-        'order' => 'ASC',
-    );
-    
-    $office_loop = new WP_Query( $office_args );
-    ?>
+    $locArgs = array('post_type' => 'location', 'posts_per_page' => -1, 'post_status' => 'publish','orderby' => 'title', 'order' => 'ASC');
+    $locLoop = new WP_Query( $locArgs ); ?>
+    <div class="location_map">
+
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDac2mOtJr_IktjUhiLZYRL_xHzxRbodRE&callback=initMap&libraries=&v=weekly" defer></script>
+
     <script>function initMap() {
-     
         const myLatlng = { lat: 34.750713, lng: -111.263263 };
-    
         const map = new google.maps.Map(document.getElementById("map"), {
             zoom: 8,
             center: myLatlng,
         });
     
-    
         var bounds = new google.maps.LatLngBounds();
         var infowindow = new google.maps.InfoWindow();  
-        
-        var markers_array = new Array();
-        var locations = [];
-        var array_holder;
-        var loc_content = "";
-        var loc_ctr = 0;
-        var emp_phones = "";
+        var markers_array = new Array(), locations = [], array_holder, loc_content = "", loc_ctr = 0, emp_phones = "";
     
-        <?php while ( $office_loop->have_posts() ) : $office_loop->the_post(); ?>
+        <?php while ( $locLoop->have_posts() ) : $locLoop->the_post(); ?>
             <?php if(!empty(get_field('map_location'))): ?>
             var loc_title = '<?php the_title(); ?>';
             var office_lat = <?php echo get_field('map_location')['lat']; ?>; 
             var office_lng = <?php echo get_field('map_location')['lng']; ?>; 
 
-            loc_content = '<div id="mapInfo'+loc_ctr+'" class="map_grid grid-container">';
-                
+            loc_content = '<div id="mapInfo'+loc_ctr+'" class="map_info">';
             loc_content += '' +loc_title+ '';
-
             loc_content += '</div>';
-    
             loc_content += '</div>';
     
             array_holder = [loc_title, office_lat, office_lng, loc_content];
@@ -61,19 +44,16 @@ function find_us_gmap(){
             loc_ctr = loc_ctr+1;
 
             <?php endif; ?>
-
             <?php endwhile; wp_reset_postdata(); ?>
         
         
         for (var i = 0; i < locations.length; i++) {
-          
           var marker = new google.maps.Marker({
             position: new google.maps.LatLng(locations[i][1], locations[i][2]),
             map: map
           });
     
            bounds.extend(marker.position);
-    
     
           google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
@@ -89,14 +69,12 @@ function find_us_gmap(){
         //now fit the map to the newly inclusive bounds
         map.fitBounds(bounds);
       
-    
         //(optional) restore the zoom level after the map is done scaling
         var listener = google.maps.event.addListener(map, "idle", function () {
             map.setZoom(8);
             google.maps.event.removeListener(listener);
         });
     
-        
         $('a.triggerMap').each(function(i){
             $(this).on('click', function(e){
                 //e.preventDefault();
@@ -104,12 +82,50 @@ function find_us_gmap(){
             });
         });
      
-    
     }</script>
     <div id="map" style="height:100vh"></div>
+    </div>
+
+    <ul class="location_states">
+    <?php
+        $location_categories = get_terms( 'locations-cat', array('orderby' => 'title', 'order' => 'ASC', 'parent' => 0, 'hide_empty' => true));
+        $loc_content = "";
+        foreach ( $location_categories as $location_category ) {
+            $args = array('posts_per_page' => -1, 'tax_query' => array( 'relation' => 'AND', array( 'taxonomy' => 'locations-cat', 'field' => 'slug', 'terms' => $location_category->slug, 'include_children' => false )
+                ), 'post_type' => 'location', 'orderby' => 'title,');
+            $locations = new WP_Query( $args ); 
+
+            $loc_content += '<li class="state_item '. $location_category->slug .'">';
+            $loc_content += '<h6 class="state_name">'. $location_category->name .'</h6>';
+                $loc_content += "<ul class='location_list'>";
+                while ( $locations->have_posts() ) { $locations->the_post();
+                    $terms = get_the_terms( $post->ID, 'location-apps' );
+
+                    ?>
+                    <li class="city_item">
+                        <a href="#"><?php the_title(); ?></a>
+                    </li>
+                    <?php
+                }
+                $loc_content += "</ul>";
+            $loc_content += "</li>";
+
+        }
+
+        return $loc_content;
+    ?>
+    </ul>
+
+    <p class="note">
+        <strong>NOTE:</strong> 
+        The map and cities below show the general geographic locations for the retail stores. Exact locations will be determined at a later date.
+    </p>
+
     <?php 
     }
-    add_shortcode('display_loc_gmap', 'find_us_gmap');
+add_shortcode('display_loc_gmap', 'find_us_gmap');
+
+
 
 
 
