affb58a658ef7ce97c6d1e194bd74bcbfcab35ed
marmbn <marfel@mybizniche.com>
2021-03-04 15:53:44 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -20,6 +20,17 @@ function find_us_gmap() {
         const map = new google.maps.Map(document.getElementById('gmap'), {
             zoom: 8,
             center: myLatlng,
+            styles: [{'featureType': 'administrative','elementType': 'all','stylers': [{'saturation': '-100'}]},
+            {'featureType': 'administrative.province', 'elementType': 'all','stylers': [ {'visibility': 'off'}]},
+            {'featureType': 'landscape', 'elementType': 'all','stylers': [{'saturation': -100},{'lightness': 65},{'visibility': 'on'}]},
+            {'featureType': 'poi', 'elementType': 'all', 'stylers': [{'saturation': -100},{'lightness': '50'},{'visibility': 'simplified'}]},
+            {'featureType': 'road','elementType': 'all','stylers': [{'saturation': '-100'}]},
+            {'featureType': 'road.highway','elementType': 'all','stylers': [{'visibility': 'simplified'}]},
+            {'featureType': 'road.arterial','elementType': 'all','stylers': [{'lightness': '30'}]},
+            {'featureType': 'road.local','elementType': 'all','stylers': [{'lightness': '40'}]},
+            {'featureType': 'transit','elementType': 'all','stylers': [{'saturation': -100},{'visibility': 'simplified'}]},
+            {'featureType': 'water','elementType': 'geometry','stylers': [{'hue': '#ffff00'},{'lightness': -25},{'saturation': -97}]},
+            {'featureType': 'water','elementType': 'labels','stylers': [{'lightness': -25},{'saturation': -100}]}]
         });
     
         var bounds = new google.maps.LatLngBounds();
