bebb277fb88b68b9a57763459b78c4800fe270a1
marmbn <marfel@mybizniche.com>
2021-03-04 16:58:25 +0800

    page default

--- page.php
+++ page.php
@@ -1,18 +1,5 @@
 <?php get_header() ?>
 
-<section class="sec_banner">
-	<div class="banner_copy">
-		<div class="grid-container">
-			<div class="grid-x grid-margin-x align-center">
-				<div class="cell large-9">
-					<h5>Eliomotors.com </h5>
-					<h2>Terms of Use for the Website</h2>
-					<p>In a nutshell, Elio is the ultimate vehicle for commuting daily. Electric vehicles, like their predecessor gas engines, are increasingly getting more expensive, with an entry level electric starting at just under $40,000. Something needed to give and Elio reversed engineered a solution. With a car entirely engineered from off-the-shelf parts, from the top suppliers in the world, a under 20,000 option was born.</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
 
 <section class="page_content"> 
 	<div class="grid-container">		
@@ -23,36 +10,6 @@
 
 		endwhile; // End of the loop.
 		?>
-		<h6>Acceptance of Terms.</h6>
-		<p>This website, at www.eliomotors.com (the “Site”), is provided by Elio Motors, Inc. (“Elio”, “Elio Motors” or “We”) subject to your acceptance of the following Terms of Service (“TOS”) as updated by Elio Motors from time to time. IF YOU DO NOT AGREE TO THESE TERMS, DO NOT USE THIS WEBSITE.</p>
-		<h6>Use of Site.</h6>
-		<p>You may view and download the materials at this Site only for your personal, non-commercial use. You may not reproduce, modify, rent, lease, loan, sell, distribute, mirror, frame, republish, download, transmit, or create derivative works of the materials on this Site, or otherwise use them for any public or commercial purpose.</p>
-		<p>You agree that you will not use the Site to: (i) transmit spam, bulk, or unsolicited communications; (ii) pretend to be Elio Motors or spoof Elio Motors s identity or misrepresent any affiliate with Elio Motors; (iii) forge headers or otherwise manipulate identifiers (including URLs) in order to disguise the origin of any content transmitted to or via the Site; or (iv) disrupt the normal flow or otherwise act in a manner that negatively affects other users’ ability to use the Site. Elio Motors reserves the right to report any wrongdoing to the applicable government agencies.</p>
-		<p>You agree you will not use any robot, spider, other automatic device, or manual process to monitor or copy the Site web pages or its content without the prior, written permission of Elio Motors.</p>
-		<h6>Cautions Regarding Forward-looking Statements</h6>
-		<p>Internet pages, investor relations releases, outlooks, presentations, audio and video files of events (live or recorded) and other documents on these internet sites contain among other things forward-looking statements that reflect management’s current views with respect to future events. The words “anticipate”, “assume”, “believe”, “estimate”, “expect”, “intend”, “may”, “plan”, “project” and “should” and similar expressions identify forward-looking statements. Such statements are subject to risks and uncertainties, including, but not limited to: changes in currency exchange rates, interest rates and in raw material prices; introduction of competing products; increased sales incentives; and decline in resale prices of used vehicles</p>
-		<h6>Cautions Regarding Forward-looking Statements</h6>
-		<p>Internet pages, investor relations releases, outlooks, presentations, audio and video files of events (live or recorded) and other documents on these internet sites contain among other things forward-looking statements that reflect management’s current views with respect to future events. The words “anticipate”, “assume”, “believe”, “estimate”, “expect”, “intend”, “may”, “plan”, “project” and “should” and similar expressions identify forward-looking statements. Such statements are subject to risks and uncertainties, including, but not limited to: changes in currency exchange rates, interest rates and in raw material prices; introduction of competing products; increased sales incentives; and decline in resale prices of used vehicles</p>
-		<h6>No Endorsement of Content.</h6>
-		<p>Elio Motors makes no representations or endorsements regarding the quality, accuracy, reliability or condition of any content posted on the Site. In addition, links on this Site to third-party websites are provided solely as a convenience. Elio Motors has not reviewed all of these third-party sites and is not responsible for any of these sites or their content. Elio Motors does not endorse, or make any representations about the information, software or other products or materials found on third party sites. If you decide to access any of the third-party websites linked to this Site, you do so entirely at your own risk.</p>
-		<h6>Trademarks, Copyright and Other Intellectual Property Provisions.</h6>
-		<p>The copyright in all materials provided on this Site is held by Elio Motors or its suppliers. You must retain all copyright and other proprietary notices contained in the materials on any copies made. Unauthorized use of any of the materials on this Site may violate copyright, trademark and other laws.</p>
-		<p>“Elio Motors Trademarks” means all names, marks, brands, logos, designs, trade dress, slogans and other designations Elio Motors uses in connection with its products and services. You agree not to incorporate any Elio Motors Trademarks into your trademarks, service marks, company names, Internet addresses, domain names, or any other similar designations without the prior, written permission of Elio Motors.</p>
-		<p>Unless explicitly stated herein, nothing in this TOS shall be construed as conferring to you any license to any Elio Motors or third party intellectual property rights, whether by estoppel, implication, or otherwise.</p>
-		<h6>Postings.</h6>
-		<p>In addition, if the Site provides areas where users can post communications, Elio Motors may but is not obligated to monitor or review the content on the Site where users post information. Elio Motors shall have the right to remove any communications from others on the Site, it its sole discretion, including materials that Elio Motors deems to be abusive, defamatory, obscene or otherwise unacceptable. Elio Motors shall have no liability related to the content in any communications by others, whether arising under the laws of copyright, libel, privacy, obscenity or otherwise.</p>
-		<h6>Disclaimer of Warranties.</h6>
-		<p>ELIO MOTORS PROVIDES THIS WEB SITE, AND ALL CONTENT AND MATERIALS ON THIS WEB SITE “AS IS” AND WITHOUT ANY WARRANTY OF ANY KIND, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING ANY IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, ACCURACY OF INFORMATIONAL CONTENT, AND NON-INFRINGEMENT. ELIO MOTORS OBLIGATIONS WITH RESPECT TO ITS PRODUCTS AND SERVICES ARE GOVERNED SOLELY BY AN AGREEMENT UNDER WHICH THE PRODUCTS AND SERVICES ARE PROVIDED. NOTHING ON THIS SITE SHOULD BE CONSTRUED TO CREATE OR MODIFY ANY SUCH AGREEMENTS. ELIO MOTORS MAKES NO WARRANTY THAT THE SITE WILL MEET YOUR REQUIREMENTS OR WILL BE UNINTERRUPTED, TIMELY, SECURE, OR ERROR FREE, OR THAT ANY DEFECTS WILL BE CORRECTED. THIS DISCLAIMER IS MADE TO THE FULLEST EXTENT PERMITTED BY LAW. SOME STATES AND COUNTRIES DO NOT ALLOW THE DISCLAIMER OF IMPLIED WARRANTIES, SO THE FOREGOING DISCLAIMER MAY NOT APPLY TO YOU.</p>
-		<h6>Prices</h6>
-		<p>All prices specified are recommended retail prices. Prices are current at the time of publication and subject to change without notice.</p>
-		<h6>Limitations of Liability.</h6>
-		<p>IN NO EVENT WHATSOEVER SHALL ELIO MOTORS, ITS PARENT COMPANIES, AFFILIATES, OR SUPPLIERS OR THEIR RESPECTIVE EMPLOYEES, SHAREHOLDERS, AGENTS, OR REPRESENTATIVES BE LIABLE FOR ANY INDIRECT, CONSEQUENTIAL, INCIDENTAL, SPECIAL, PUNITIVE OR EXEMPLARY DAMAGES, OR FOR ANY LOSS OF PROFITS OR REVENUE, INCLUDING BUT NOT LIMITED TO LOSS OF SALES, PROFIT, REVENUE, GOODWILL, OR DOWNTIME, (ARISING UNDER TORT, CONTRACT, OR OTHER LAW), REGARDLESS OF SUCH PARTY’S NEGLIGENCE OR WHETHER SUCH PARTY KNEW OR SHOULD HAVE KNOWN OF THE POSSIBILITY OF SUCH DAMAGES. YOU UNDERSTAND AND AGREE THAT THE DOWNLOAD AND UPLOAD OF ANY MATERIAL THROUGH THIS SITE IS DONE AT YOUR DISCRETION AND RISK AND THAT YOU ARE SOLELY RESPONSIBLE FOR ANY LOSS OR DAMAGE TO YOUR COMPUTER SYSTEM OR LOSS OF DATA THAT MAY RESULT FROM THE DOWNLOAD OR UPLOAD OF ANY MATERIAL. ELIO MOTORS ASSUMES NO LIABILITY IN CONNECTION WITH THE PROVISION OF THE SITE TO THE FULLEST EXTENT PERMITTED BY LAW. Some states or other jurisdictions do not allow the exclusion or limitation of liability for incidental or consequential damages, so the above limitations and exclusions may not apply to you.</p>
-		<h6>Submissions.</h6>
-		<p>Any material, information or idea you transmit to Elio Motors or post on this Site will be treated as non-confidential and non-proprietary. Elio Motors will have no obligations with respect to such submissions and Elio Motors will be free to copy, disclose, distribute, incorporate and otherwise use any such submission for any and all commercial or non-commercial purposes.</p>
-		<h6>Choice of Law; Jurisdiction.</h6>
-		<p>Elio Motors administers this Site from its offices in Denver, Colorado, USA. Elio Motors makes no representation that materials or services at this Site are appropriate or available for use outside the United States. If you choose to access this Site from outside the United States, you are responsible for compliance with all applicable local laws. This TOS shall be governed by and construed in accordance with the laws of the state of Colorado exclusive of its choice of law or conflicts of law rules. The exclusive venue for all litigation regarding or arising out of the Site shall be in Denver County, Colorado and you agree to submit to the jurisdiction of the courts Denver County, Colorado for such litigation.</p>
-		<h6>Miscellaneous.</h6>
-		<p>In the event that one or more portions of this TOS shall, for any reason, be held to be invalid, illegal or unenforceable in any respect, such validity, illegality or unenforceability shall not affect any other provision contained in this TOS. Any delay or failure to require performance of any provision hereof shall in no manner affect any right at a later time to enforce such provision. Elio Motors may revise this TOS at any time by updating this posting. You should visit this page from time to time to review the then-current TOS because it will be binding on you.</p>
 	</div>	
 </section> 
 
