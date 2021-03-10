7044dd3d3a980bb4064e1ca2061418638515d042
marmbn <marfel@mybizniche.com>
2021-03-11 07:04:43 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -156,6 +156,7 @@ add_filter('upload_mimes', 'mbn_myme_types');
 
 // Post Reading Time
 function reading_time() {
+    global $post;
     $content = get_post_field( 'post_content', $post->ID );
     $word_count = str_word_count( strip_tags( $content ) );
     $readingtime = ceil($word_count / 200);
